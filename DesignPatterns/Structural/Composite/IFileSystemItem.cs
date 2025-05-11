e3f7a97bb9fdec9893f0ba36debf936792351086
Ravikumar Makwana <17comp.ravi.makwana@gmail.com>
2025-05-11 20:50:58 +0530

    Implement Composite Design Pattern

---
+++ DesignPatterns/Structural/Composite/IFileSystemItem.cs
@@ -0,0 +1,48 @@
+﻿namespace DesignPatterns.Structural.Composite
+{
+    public interface IFileSystemItem
+    {
+        void Display(string indent = "");
+    }
+
+    public class File : IFileSystemItem
+    {
+        private readonly string _name;
+
+        public File(string name)
+        {
+            _name = name;
+        }
+
+        public void Display(string indent = "")
+        {
+            Console.WriteLine($"{indent}- File: {_name}");
+        }
+    }
+
+    public class Folder : IFileSystemItem
+    {
+        private readonly string _name;
+        private readonly IList<IFileSystemItem> _fileSystemItems;
+
+        public Folder(string name)
+        {
+            _name = name;
+            _fileSystemItems = new List<IFileSystemItem>();
+        }
+
+        public void Add(IFileSystemItem fileSystemItem)
+        {
+            _fileSystemItems.Add(fileSystemItem);
+        }
+
+        public void Display(string indent = "")
+        {
+            Console.WriteLine($"{indent}+ Folder: {_name}");
+            foreach (IFileSystemItem fileSystemItem in _fileSystemItems)
+            {
+                fileSystemItem.Display(indent + " ");
+            }
+        }
+    }
+}
