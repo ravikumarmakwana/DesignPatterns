9e793e4d7aee2b1cdef6271dcd046c11d3a0bc9c
Ravikumar Makwana <17comp.ravi.makwana@gmail.com>
2025-05-11 20:51:30 +0530

    Implement Chain Of Responsibility Design Pattern

---
+++ DesignPatterns/Behavioral/ChainOfResponsibility/IMiddleware.cs
@@ -0,0 +1,55 @@
+﻿namespace DesignPatterns.Behavioral.ChainOfResponsibility
+{
+    public interface IMiddleware
+    {
+        void Invoke(string message);
+    }
+
+    public class LogMiddleware : IMiddleware
+    {
+        private IMiddleware _next;
+
+        public void SetNext(IMiddleware next)
+        {
+            _next = next;
+        }
+
+        public void Invoke(string message)
+        {
+            Console.WriteLine($"Log Middleware: {message}");
+            _next?.Invoke(message);
+        }
+    }
+
+    public class EmailMiddleware : IMiddleware
+    {
+        private IMiddleware _next;
+
+        public void SetNext(IMiddleware next)
+        {
+            _next = next;
+        }
+
+        public void Invoke(string message)
+        {
+            Console.WriteLine($"Email Middleware: {message}");
+            _next?.Invoke(message);
+        }
+    }
+
+    public class SMSMiddleware: IMiddleware
+    {
+        private IMiddleware _next;
+
+        public void SetNext(IMiddleware next)
+        {
+            _next = next;
+        }
+
+        public void Invoke(string message)
+        {
+            Console.WriteLine($"SMS Middleware: {message}");
+            _next?.Invoke(message);
+        }
+    }
+}
